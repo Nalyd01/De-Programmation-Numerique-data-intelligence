6a414166f6c70847147e946d9a4ea167a3821f36
Nalyd01 <Nalyd110698>
2020-03-25 21:23:42 +0100

    Ajout des méthodes d'affichage

--- LibrairieClassificationStatistics/Principal.c
+++ LibrairieClassificationStatistics/Principal.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
-#define NB_MAX_CLASSES 15
+#define NB_MAX_CLASSES 100
 
 typedef struct classe {
-	int numClasse; 
-	int nbRep;
-	int nbGoodAnswers;
+	int numClasse; // Class number (which corresponds to an activity)
+	int nbRep; // Number of class occurrences in the table realClasses
+	int nbGoodAnswers; // Number of goog answers
 } Classe;
 
-int researchClasses(int realClasses[],Classe distinctClasses[]);
+int researchClasses(int realClasses[],Classe distinctClasses[], int size);
+void displayResultsForEachClasses(int realClasses[], int estimateClasses[], int size);
+void goodAnswers(int realClasses[], int estimateClasses[], int size, Classe distinctClasses[], int sizeDistinctClasse);
+double calculatePercentage(int nbRep, int nbGoodAnswers);
+void displayAccuracy(int realClasses[], int estimateClasses[], int size); 
+double calculateAccuracy(Classe distinctClasses[], int size);
 
 void main(void) {
-	int realClasses[NB_MAX_CLASSES] = { 5, 2, 5, 3, 5, 3, 2, 4, 7, 9, 3, 1, 13, 0, 5 };
-	int estimateClasses[NB_MAX_CLASSES] = { 5, 5, 1, 2, 1, 3, 2, 4, 7, 9, 3, 13, 1, 5, 3 };
+	int realClasses[8] = { 5, 2, 5, 3, 5, 3, 2, 4 };
+	int estimateClasses[8] = { 5, 5, 1, 2, 1, 3, 2, 4 };
 	
-	/*Test function researchClasses
-	Classe distinctClasses[NB_MAX_CLASSES];
-	int nbDistinctClasses = researchClasses(realClasses, distinctClasses);
-	for (int posDistinctClasses = 0;posDistinctClasses < nbDistinctClasses;posDistinctClasses++) {
-		printf("NumClasse = %d", distinctClasses[posDistinctClasses].numClasse);
-		printf("| NbRep = %d", distinctClasses[posDistinctClasses].nbRep);
-		printf("| NbGoodAnswers = %d\n", distinctClasses[posDistinctClasses].nbGoodAnswers);
-	}
-	printf("Nombre de classes = %d", nbDistinctClasses);
-	getchar();
-	*/
+	displayResultsForEachClasses(realClasses, estimateClasses, 8); 
+	displayAccuracy(realClasses, estimateClasses, 8); 
 }
 
-int researchClasses(int realClasses[], Classe distinctClasses[]) {
+int researchClasses(int realClasses[], Classe distinctClasses[], int size) {
 	int currentRealClasse = 0;
 	int realClasse;
 	int currentDistinctClasse;
 	int nbDistinctClasses = 0;
 	int posDistinctClasses;
-	while (currentRealClasse < NB_MAX_CLASSES) {
+	while (currentRealClasse < size) {
 		realClasse = realClasses[currentRealClasse];
 		currentDistinctClasse = 0;
 		while (currentDistinctClasse < nbDistinctClasses && realClasse > distinctClasses[currentDistinctClasse].numClasse)
@@ -58,4 +55,66 @@ int researchClasses(int realClasses[], Classe distinctClasses[]) {
 		currentRealClasse++;
 	}
 	return nbDistinctClasses;
+}
+
+void displayResultsForEachClasses(int realClasses[], int estimateClasses[], int size) {
+	double percentage;
+	int sizeDistinctClasses; 
+	Classe distinctClasses[NB_MAX_CLASSES]; 
+
+	sizeDistinctClasses = researchClasses(realClasses, distinctClasses, size); 
+
+	goodAnswers(realClasses, estimateClasses, size, distinctClasses, sizeDistinctClasses);
+
+	printf("      Classe\t|   Bien classes   |\tTotal\t|\tPourcentage\n");
+
+	for (int i = 0; i < sizeDistinctClasses; i++) {
+		percentage = calculatePercentage(distinctClasses[i].nbRep, distinctClasses[i].nbGoodAnswers);
+
+		printf("\t%d\t|\t %d \t   |\t  %d\t|\t  %.2f%%\n", distinctClasses[i].numClasse, distinctClasses[i].nbGoodAnswers, distinctClasses[i].nbRep, percentage);
+	}
+
+	system("pause");
+}
+
+void goodAnswers(int realClasses[], int estimateClasses[], int size, Classe distinctClasses[], int sizeDistinctClasses) {
+	for (int i = 0; i < size; i++) {
+
+		if (realClasses[i] == estimateClasses[i]) {
+
+			for (int j = 0; j < sizeDistinctClasses; j++) {
+
+				if (distinctClasses[j].numClasse == realClasses[i]) {
+					distinctClasses[j].nbGoodAnswers++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+double calculatePercentage(int nbRep, int nbGoodAnswers) {
+	return (double)nbGoodAnswers / nbRep * 100;
+}
+
+void displayAccuracy(int realClasses[], int estimateClasses[], int size) {
+	Classe distinctClasses[NB_MAX_CLASSES]; 
+
+	int sizeDistinctClasses = researchClasses(realClasses, distinctClasses, size); 
+	goodAnswers(realClasses, estimateClasses, size, distinctClasses, sizeDistinctClasses);
+
+	double accuracy = calculateAccuracy(distinctClasses, sizeDistinctClasses);
+
+	printf("L'accuracy est de %.2f%%\n", accuracy);
+	system("pause");
+}
+
+double calculateAccuracy(Classe distinctClasses[], int sizeDistinctClasses) {
+	double sum = 0;
+
+	for (int i = 0; i < sizeDistinctClasses; i++) {
+		sum += calculatePercentage(distinctClasses[i].nbRep, distinctClasses[i].nbGoodAnswers);
+	}
+	 
+	return sum / sizeDistinctClasses;
 }
