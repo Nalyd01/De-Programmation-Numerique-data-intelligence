3f318f8922f0d98fcef06a1de1115fa59776ae3c
Nalyd01 <Nalyd110698>
2020-03-26 11:29:44 +0100

    test

--- LibrairieClassificationStatistics/Principal.c
+++ LibrairieClassificationStatistics/Principal.c
@@ -19,6 +19,8 @@ double calculateAccuracy(Classe distinctClasses[], int size);
 void main(void) {
 	int realClasses[8] = { 5, 2, 5, 3, 5, 3, 2, 4 };
 	int estimateClasses[8] = { 5, 5, 1, 2, 1, 3, 2, 4 };
+
+	displayResultsForEachClasses(realClasses, estimateClasses, 8);
 }
 
 int researchClasses(int realClasses[], Classe distinctClasses[], int size) {
